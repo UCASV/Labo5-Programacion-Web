158cbffdce54b32e55d9518ce092bc52ac9c15fd
Sermeno26 <00030022@uca.edu.sv>
2025-10-03 10:47:53 -0600

    :construction: Agregar ejercicios 2, 3, 4, 5

---
+++ my-app-guia-5/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function MyButton(props) {
+  return (
+    <button onClick={props.Fn}>
+      Soy un boton custom y mi cuenta es: {props.count}
+    </button>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount((count) => count + 1);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      
+      <div className="card">
+        <MyButton count={count} Fn={increment} /> 
+
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App
