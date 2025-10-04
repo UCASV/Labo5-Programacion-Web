487fdcbe49f7a2ef5a4a4fcd90d842c2aded4ec8
Sermeno26 <00030022@uca.edu.sv>
2025-10-03 19:11:10 -0600

    :sparkles: Agregar ejercicio final

--- my-app-guia-5/src/App.jsx
+++ my-app-guia-5/src/App.jsx
@@ -3,42 +3,68 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function MyButton(props) {
+function CienficosCartas({ cientificos }) { 
   return (
-    <button onClick={props.Fn}>
-      Soy un boton custom y mi cuenta es: {props.count}
-    </button>
+    <div className="scientist-card">
+      <h2>{cientificos.name}</h2>
+      
+      <div className="card-content">
+        <img
+          src={cientificos.image}
+          alt={`Foto de ${cientificos.name}`}
+          className="scientist-photo"
+        />
+        
+        <ul>
+          <li>
+             <a>Profesion: </a>{cientificos.profession}
+          </li>
+          <li>
+            <a>Premios:</a>  {cientificos.awards} {cientificos.awardsList}
+          </li>
+          <li>
+            <a>Descubrio:</a> {cientificos.discovery}
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
+const DatosCientificos =[
+  {
+    id: 1,
+    name: 'Maria Skłodowska-Curie',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/5/51/Marie_Curie_%281900%29.jpg', // URL de imagen de ejemplo
+    profession: 'física y química',
+    awards: 4,
+    awardsList: '(Premio Nobel de Física, Premio Nobel de Química, Medalla Davy, Medalla Matteucci)',
+    discovery: 'polonio (elemento químico)'
+  },
+  {
+    id: 2,
+    name: 'Katsuko Saruhashi',
+    image: 'https://i.imgur.com/OKS67lh.jpg', // URL de imagen de ejemplo
+    profession: 'geoquímica',
+    awards: 2,
+    awardsList: '(Premio Miyake de geoquímica, Premio Tanaka)',
+    discovery: 'un método para medir el dióxido de carbono en el agua de mar'
+  },
+];
+
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => setCount((count) => count + 1);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      
-      <div className="card">
-        <MyButton count={count} Fn={increment} /> 
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Cientificos Notables</h1>
+      {/* Recorro o mapeo todo de DatosCientificos*/}
+      <section className="scientists-list">
+        {DatosCientificos.map(cientificos => (
+          <CienficosCartas 
+            key={cientificos.id} 
+            cientificos={cientificos} 
+          />
+        ))}
+      </section>      
     </>
   );
 }
